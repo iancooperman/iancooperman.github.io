0b75ff1a420ba2d7b21146b72b4aaa89cf040021
Ian Cooperman <i.perry.leonard.c@gmail.com>
2021-07-11 15:33:55 -0700

    Spruced up the homepage by adding lorem text and resizing profile pic

--- src/Home.js
+++ src/Home.js
@@ -1,21 +1,33 @@
 import profilePic from './profilepic.jpg'
+import './Home.css';
 
 import Jumbotron from 'react-bootstrap/Jumbotron';
 import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+import AboutMe from './AboutMe.js';
 
 function Home() {
     return (
         <div>
             <Jumbotron>
                 <Container>
-                    <h1>Hi. I'm Ian Cooperman.</h1>
-                    <p>I'm a recently graduated Computer Science student.</p>
-                    <img src={profilePic} alt="Profile Pic"></img>
+                    <Row>
+                        <Col>
+                            <h1>Hi. I'm Ian Cooperman.</h1>
+                            <p>I'm a recently graduated Computer Science student.</p>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Tortor condimentum lacinia quis vel eros donec. Ornare arcu odio ut sem nulla.</p>
+                        </Col>
+                        <Col>
+                            <img src={profilePic} alt="Profile Pic" id="profilePic"></img>
+                        </Col>
+                    </Row>
                 </Container>
             </Jumbotron>
 
             <Container>
-                <h2>About Me</h2>
+                <AboutMe />
             </Container>
         </div>
     );
