4d8c484a5c20c882ec956382654f7ee530694cf4
Ian Cooperman <i.perry.leonard.c@gmail.com>
2021-09-02 10:09:12 -0700

    Finished porting

--- src/pages/JavaScriptGraphicsEngine.js
+++ src/pages/JavaScriptGraphicsEngine.js
@@ -35,45 +35,64 @@ function JavaScriptGraphicsEngine(props) {
                 <p>The <code>GameObject</code> class (named similarly to the Unity analogue) is the bread and butter of this engine. It implements the scene graph structure by allowing the attachment of other <code>GameObject</code>s, which get rendered every frame in a traversal of the resulting tree.</p>
 
                 <pre>
-                    (`class GameObject: {
-                        constructor() {
-                        // Array of GameObjects
-                        this._children = [];
+                    {`
+                        class GameObject: {
+                            constructor() {
+                                // Array of GameObjects
+                                this._children = [];
 
-                    // Array of GameComponents
-                    this._components = [];
-                    this._transform = new Transform();
-                    this._engine = null;
-    }
+                                // Array of GameComponents
+                                this._components = [];
+                                this._transform = new Transform();
+                                this._engine = null;
+                            }
 
-                    update() {
-        for (var i = 0; i < this._components.length; i++) {
-                        this._components[i].update();
-        }
-    }
+                            update() {
+                                for (var i = 0; i < this._components.length; i++) {
+                                    this._components[i].update();
+                                }
+                            }
 
-                    render(renderingEngine) {
-        for (var i = 0; i < this._components.length; i++) {
-                        this._components[i].render(renderingEngine);
-        }
-    }
+                            render(renderingEngine) {
+                                for (var i = 0; i < this._components.length; i++) {
+                                    this._components[i].render(renderingEngine);
+                                }
+                            }
 
-                    updateAll() {
-                        this.update();
-                    for (var i = 0; i < this._children.length; i++) {
-                        this._children[i].updateAll();
-        }
-    }
+                            updateAll() {
+                                this.update();
+                                for (var i = 0; i < this._children.length; i++) {
+                                    this._children[i].updateAll();
+                                }
+                            }
 
-                    renderAll(renderingEngine) {
-                        this.render(renderingEngine);
-                    for (var i = 0; i < this._children.length; i++) {
-                        this._children[i].renderAll(renderingEngine);
-        }
-    }
-}`)
+                            renderAll(renderingEngine) {
+                                this.render(renderingEngine);
+                                for (var i = 0; i < this._children.length; i++) {
+                                    this._children[i].renderAll(renderingEngine);
+                                }
+                            }
+                        }
+                    `}
                 </pre>
 
+                <p>If I were to create another engine, there are plenty of things I would do differently. For one, I would represent objects' rotation with Quaternions instead of a Vector, as Quaternion operations are more performant. However, perhaps the biggest regret I have with this implementation is the lack of proper lighting. There's a reason the scene ended up fully bright. I spent so much time putting the scene graph structure together that I didn't have enough time to write proper lighting code. Here's how rotation is currently handled in <code>transform.js</code>.</p>
+
+                <pre>
+                    {`
+                        rotate(axis, angle) {
+                            if (axis === "x") {
+                                vec3.add(this.rot, this.rot, vec3.fromValues(angle, 0, 0));
+                            }
+                            else if (axis === "y") {
+                                vec3.add(this.rot, this.rot, vec3.fromValues(0, angle, 0));
+                            }
+                            else if (axis === "z") {
+                                vec3.add(this.rot, this.rot, vec3.fromValues(0, 0, angle));
+                            }
+                        }
+                    `}
+                </pre>
             </Container>
         </div>
     );
